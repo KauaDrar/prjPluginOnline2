9a4e0bdb7bbb9099d980076af883e2b7af24d58f
KauaDrar <kauadosreisakamine@hotmail.com>
2021-06-04 15:18:03 +0900

    Finalizei o aplicativo, adicionando o plugin de leitura de QR Code, estilizando no CSS, e linkando o resultado do QR Code a uma textarea.

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,2 +1,25 @@
 // This is a JavaScript file
 
+$(document).on("click","#codigo",function(){
+  cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          document.getElementById('text').value = result.text;
+      },
+      function (error) {
+          alert("O scan falhou: " + error);
+      },
+      {
+          preferFrontCamera : false, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: true, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Place a barcode inside the scan area", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+   );
+});
